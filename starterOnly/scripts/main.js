4c1002a944142e7954e533fe0ff5849d5286f947
johndole <isildur64500@gmail.com>
2024-01-22 14:51:13 +0100

    Created main.js as a starter point for our landingpage features.

---
+++ starterOnly/scripts/main.js
@@ -0,0 +1,9 @@
+import { modal,} from "./features/modal.js";
+import { validation } from "./features/validation.js";
+
+
+
+document.addEventListener('DOMContentLoaded', async function () {
+    modal();
+    validation();
+});
